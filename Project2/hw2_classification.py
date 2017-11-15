9544377b2043a4c9bb17463ef3510eaa6b0c5d00
edgarmedina1801 <edgarmedina1801@gmail.com>
2017-11-15 21:50:03 -0200

    Update hw2_classification.py

--- Project2/hw2_classification.py
+++ Project2/hw2_classification.py
@@ -3,6 +3,60 @@ from sklearn.metrics import confusion_matrix
 import numpy as np
 import sys
 
+
+def ReadDataset():
+    # # FOR TESTING WITH REAL DATA.
+    import pandas as pd
+    # # Load CSV and columns
+    df = pd.read_csv("iris.csv")
+    data = df[list(df.columns.values)]
+    ind = np.arange(len(df))
+    np.random.shuffle(ind)
+    y = df['species']
+    X = df[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+    for c,ik in enumerate(y.unique()):
+        y[y==ik] = c
+    X = X.get_values()[ind]
+    y = y.get_values()[ind]
+    # # Split the data into training/testing sets
+    X_train = X[:-75]
+    X_test = X[-75:]
+    # # Split the targets into training/testing sets
+    y_train = y[:-75]
+    y_test = y[-75:]
+    np.savetxt("y_train.csv", y_train, delimiter=",") # write output to file
+    np.savetxt("X_train.csv", X_train, delimiter=",") # write output to file
+    np.savetxt("X_test.csv", X_test, delimiter=",") # write output to file
+    np.savetxt("y_test.csv", y_test, delimiter=",") # write output to file
+
+def CreateDataset():
+    from sklearn.datasets import make_moons, make_circles, make_classification
+    X, y = make_classification(n_samples=8000, n_classes=10, n_features=20, n_redundant=0, n_informative=8,
+                               random_state=0, n_clusters_per_class=1)
+    rng = np.random.RandomState(2)
+    X += 0.1 * rng.uniform(size=X.shape)
+    linearly_separable = (X, y)
+
+    ind = np.arange(len(y))
+    np.random.shuffle(ind)
+    X = X[ind]
+    y = y[ind]
+    # # Split the data into training/testing sets
+    X_train = X[:-1000]
+    X_test = X[-1000:]
+    # # Split the targets into training/testing sets
+    y_train = y[:-1000]
+    y_test = y[-1000:]
+
+
+    np.savetxt("y_train.csv", y_train, delimiter=",") # write output to file
+    np.savetxt("X_train.csv", X_train, delimiter=",") # write output to file
+    np.savetxt("X_test.csv", X_test, delimiter=",") # write output to file
+    np.savetxt("y_test.csv", y_test, delimiter=",") # write output to file
+
+############# CODE FOR CLASSIFICATION AFTER THIS POINT #############
+# The code before this point is not necessary to run this file.
+
 # X_train = np.genfromtxt(sys.argv[1], delimiter=",")
 # y_train = np.genfromtxt(sys.argv[2])
 # X_test = np.genfromtxt(sys.argv[3], delimiter=",")
